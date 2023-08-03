96ac5845a4849ac2473e921f68798bb6adb1d66e
Glenn Moloney <glenn.moloney@gmail.com>
2023-08-03 14:04:57 +1000

    espnow_scan.py: More robust procedure for identifying correct channel.
    Rather than wait for any detectiion on a channel, measure the ping response rate
    for every channel and set the channel to that with the max response
    rate.

--- src/espnow_scan.py
+++ src/espnow_scan.py
@@ -1,16 +1,29 @@
 import sys
+import time
 import network
 import espnow
 
+MAX_CHANNEL = 14  # Maximum wifi channel to scan (2.4MHz band)
+NUM_PINGS = 10  # The default number of pings to send on each channel
+PING_MSG = b"ping"  # The message to send in each packet to the peer
+CHANNEL_SETTLING_TIME = 0.0  # Delay after setting channel before testing (seconds)
+MIN_PING_RESPONSE_PC = 10  # Minimum acceptable ping response rate (percent)
+
 sta, ap = (network.WLAN(i) for i in (network.STA_IF, network.AP_IF))
+
 
 def set_channel(channel):
     if sta.isconnected():
         raise OSError("can not set channel when connected to wifi network.")
     if ap.isconnected():
-        raise OSError("can not set channel when clients are connected to AP.")
+        raise OSError("can not set channel when clients are connected to AP_IF.")
     if sta.active() and sys.platform != "esp8266":
-        sta.config(channel=channel)  # On ESP32 use STA interface
+        try:
+            sta.config(channel=channel)  # On ESP32 use STA interface
+        except RuntimeError as err:
+            if channel < 12:
+                print(f"Error setting channel: {err}")
+            return None
         return sta.config("channel")
     else:
         # On ESP8266, use the AP interface to set the channel of the STA interface
@@ -21,48 +34,63 @@ def set_channel(channel):
         return ap.config("channel")
 
 
-def scan(peer, retries=5):
+# Return the fraction of pings to peer which succeed.
+def ping_peer(enow, peer, channel, num_pings, verbose):
+    if set_channel(channel) is None:
+        return 0.0
+    time.sleep(CHANNEL_SETTLING_TIME)
+    msg = PING_MSG + bytes([channel])
+    frac = sum((enow.send(peer, msg) for _ in range(num_pings))) / num_pings
+    if verbose:
+        print(f"Channel {channel:2d}: ping response rate = {frac * 100:3.0f}%.")
+    return frac
+
+
+def scan(peer, num_pings=NUM_PINGS, verbose=False):
     """Scan the wifi channels to find the given espnow peer device.
 
     If the peer is found, the channel will be printed and the channel number
     returned.
     Will:
         - scan using the STA_IF;
-        - turn off the AP_IF when finished (on esp8266); and
         - leave the STA_IF running on the selected channel of the peer.
 
     Args:
         peer (bytes): The MAC address of the peer device to find.
-        retries (int, optional):
-            Number of times to attempt to send for each channel. Defaults to 5.
+        num_pings (int, optional):
+            Number of pings to send for each channel. (default=20).
+        verbose (bool): Print results of each channel test.
 
     Returns:
         int: The channel number of the peer (or 0 if not found)
     """
+    if not sta.active() and not ap.active():
+        sta.active(True)  # One of the WLAN interfaces must be active
     enow = espnow.ESPNow()
     enow.active(True)
     try:
         enow.add_peer(peer)  # If user has not already registered peer
     except OSError:
         pass
-    found = []
-    for channel in range(1, 15):
-        set_channel(channel)
-        for _ in range(retries):
-            if enow.send(peer, b'ping'):
-                found.append(channel)
-                print(f"Found peer {peer} on channel {channel}.")
-                break
-                # return channel
-    if not found:
+
+    # A list of the ping success rates (fraction) for each channel
+    ping_fracs = [
+        ping_peer(enow, peer, channel, num_pings, verbose)
+        for channel in range(1, MAX_CHANNEL + 1)
+    ]
+    max_frac = max(ping_fracs)
+    if max_frac < (MIN_PING_RESPONSE_PC + 5) / 100:
+        print(f"No channel found with response rate above {MIN_PING_RESPONSE_PC}%")
         return 0
-    # Because of channel cross-talk we expect more than one channel to be found
+    # Get a list of channel numbers within 5% of max ping success rate
+    found = [chan + 1 for chan, rate in enumerate(ping_fracs) if rate >= max_frac - 0.05]
+
+    # Because of channel cross-talk we may get more than one channel to be found
     # If 3 channels found, select the middle one
     # If 2 channels found: select first one if it is channel 1 else second
     # If 1 channels found, select it
     count = len(found)
-    index = 0 if count == 1 or (count == 2 and found[0] == 1) else 1
+    index = (count // 2) if not (count == 2 and found[0] == 1) else 0
     channel = found[index]
-    print(f"Setting wifi channel to {channel}")
-    set_channel(channel)
-    return channel
+    print(f"Setting wifi radio to channel {channel} ({max_frac * 100:3.0f}% response)")
+    return set_channel(channel)
